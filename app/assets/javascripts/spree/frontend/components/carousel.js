172140b3ba7bc9e6d988dfd488324457df9b0d42
Ilyeo <jrag.tkd@gmail.com>
2017-10-06 23:53:44 -0500

    Integration of owl plugin to project for build a carousel

---
+++ app/assets/javascripts/spree/frontend/components/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    navigation: true,
+    loop: true,
+    items: 1,
+    margin: 0,
+    dots: true,
+    singleItem:true,
+  })
+});
