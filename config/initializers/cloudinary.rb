86ed2f113d03b71922940d1ad71d95cfbd252573
Ilyeo <jrag.tkd@gmail.com>
2017-10-10 11:03:01 -0500

    Add config files for integration with cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dzgzfvmus'
+  config.api_key = '118644846692956'
+  config.api_secret = 'EXtL55XJQ6rDZY7V3zOEpwXhoBg'
+  config.cdn_subdomain = true
+end
